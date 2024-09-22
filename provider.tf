9a5ca1d5fe0681cb0c6d358b946b8fd976e55ea5
udaybuilds <udaysrivastava0@gmail.com>
2024-09-22 17:24:46 +0530

    Basic File first push

--- provider.tf
+++ provider.tf
@@ -10,8 +10,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  client_id       = "9a00a4ef-001d-4cca-8592-0d80c927bedc"
-  client_secret   = "CBH8Q~gYcPKgVAFY2X0E9TTbSuxhUOo~LFfltcHl"
-  tenant_id       = "4063e75f-79cf-4bc3-af5e-6530dfcbeb2d"
-  subscription_id = "39a35ad4-a385-4500-9860-a69ff84b8b70"
+  #Uncomment these with your values 
+  #client_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  #client_secret   = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  #tenant_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  #subscription_id = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 }
